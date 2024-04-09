57123dac349adc09c122014679f5c6fcc8eb8579
Brechristian <breshearschristian@hotmail.com>
2024-04-08 20:34:16 -0600

    committe d broken stuff

--- IntexII_Project_4_2/Controllers/HomeController.cs
+++ IntexII_Project_4_2/Controllers/HomeController.cs
@@ -36,10 +36,75 @@ namespace IntexII_Project_4_2.Controllers
             return View(ProductList);
         }
 
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        public IActionResult AllOrders()
+        {
+            return View();
+        }
+        public IActionResult AllProducts()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult AdminIndex()
+        {
+            return View();
+        }
+        public IActionResult UpdateProduct()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Confirmation0()
+        {
+            return View();
+        }
+        public IActionResult Confirmation1()
+        {
+            return View();
+        }
+        public IActionResult RegistrationSuccessful()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult CartSummary()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
         }
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult ViewProducts()
+        {
+            return View();
+        }
+
 
     }
 }
