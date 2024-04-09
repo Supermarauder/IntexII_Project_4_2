ce33a93a0177c59ee9e578eb32456995115cac6e
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-04-09 09:16:01 -0600

    Scaffolded the database into the project

--- IntexII_Project_4_2/Models/Product.cs
+++ IntexII_Project_4_2/Models/Product.cs
@@ -1,14 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace IntexII_Project_4_2.Models
+namespace IntexII_Project_4_2.Models;
+
+public partial class Product
 {
-    public class Product
-    {
-        [Key]
-        public int ProductId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public int Price { get; set; }
-    }
+    [Key]
+    public int ProductId { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Year { get; set; }
+
+    public int? NumParts { get; set; }
+
+    public int? Price { get; set; }
+
+    public string? ImgLink { get; set; }
+
+    public string? PrimaryColor { get; set; }
+
+    public string? SecondaryColor { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Category { get; set; }
 }
